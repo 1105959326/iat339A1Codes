9fffb413a12e5b3ffe1d62143af2f33324bd544a
1105959326 <69542207+1105959326@users.noreply.github.com>
2021-03-27 16:29:13 -0700

    revert1

--- Pattern Library/js/javascript.js
+++ Pattern Library/js/javascript.js
@@ -1,21 +1,11 @@
 'use strict';
 var sidenav = document.getElementsByClassName("sideNavi")[0];
-var btn = document.getElementsByClassName("opennavi")[0];
+console.log('Hello World');
+function openNav(){
+	.style.display = "block";
+	document.getElementsByClassName("sideNavi")[0].fadeIn(1000);
+}
 
-
-btn.addEventListener('click', 
-	function(){
-	if(sidenav.classList.contains('hide')){
-		console.log("none");
-		sidenav.classList.remove('hide');
-		setTimeout(function (){
-			sidenav.classList.remove('visualhide')
-		}, 20);
-	}
-	else{
-		console.log("yes");
-		sidenav.classList.add('visualhide');
-		sidenav.classList.add('hide');
-	}
-	}, false);
-
+function closeNav(){
+	document.getElementsByClassName("sideNavi")[0].style.display = "none";
+}
