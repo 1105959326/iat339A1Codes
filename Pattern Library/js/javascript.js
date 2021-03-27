eb5e77afe360114cf5adaaca289a987f175792dc
1105959326 <69542207+1105959326@users.noreply.github.com>
2021-03-27 16:59:26 -0700

    jsFix

--- Pattern Library/js/javascript.js
+++ Pattern Library/js/javascript.js
@@ -1,11 +1,18 @@
 'use strict';
 var sidenav = document.getElementsByClassName("sideNavi")[0];
-console.log('Hello World');
-function openNav(){
-	.style.display = "block";
-	document.getElementsByClassName("sideNavi")[0].fadeIn(1000);
-}
-
-function closeNav(){
-	document.getElementsByClassName("sideNavi")[0].style.display = "none";
-}
+var btn = document.getElementsByClassName("opennavi")[0];
+btn.addEventListener('click', 
+	function(){
+	if(sidenav.classList.contains('hide')){
+		console.log("none");
+		sidenav.classList.remove('hide');
+		setTimeout(function (){
+			sidenav.classList.remove('visualhide')
+		}, 20);
+	}
+	else{
+		console.log("yes");
+		sidenav.classList.add('visualhide');
+		sidenav.classList.add('hide');
+	}
+	}, false);
